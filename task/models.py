c4af1337d23c8deeebed14b92719d7e0b3d65f99
Danial Erfanian <erfaniiyanerfan@gmail.com>
2023-03-05 20:50:57 +0330

    Add User model migrations

--- task/models.py
+++ task/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 
 class User(AbstractUser):
+    groups = None
+    user_permissions = None
     phone_number = models.CharField(max_length=200)
     avatar = models.FileField(upload_to='uploads/avatars/')
     themes = (
