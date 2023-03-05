9a0e59891b5f13e0a0f3727c955a04876f40c857
Danial Erfanian <erfaniiyanerfan@gmail.com>
2023-03-05 20:48:57 +0330

    Add User model

--- task/models.py
+++ task/models.py
@@ -1,4 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-# Create your models here.
+class User(AbstractUser):
+    phone_number = models.CharField(max_length=200)
+    avatar = models.FileField(upload_to='uploads/avatars/')
+    themes = (
+        ('light', 'Light Theme'),
+        ('dark', 'Dark Theme'),
+    )
+    theme = models.CharField(max_length=255, choices=themes)
