2b8237f665c974fc8ca3cf8f3df7e0aac9c0ed52
salehshoji <mohamadsaleh.shojai@yahoo.com>
2023-03-09 12:15:01 +0330

    add create new task test

--- task/tests.py
+++ task/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from task.models import User
+from task.models import User, Task
 
 
 # Create your tests here.
@@ -15,3 +15,21 @@ class UserTest(TestCase):
                                                 "password2": "a;ljf034"
                                                 })
         self.assertEqual(User.objects.count(), 1)
+
+
+class TaskTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        User.objects.create_user("user1", password="a;ljf034")
+
+    def test_create_task(self):
+        self.client.login(username="user1", password="a;ljf034")
+        response = self.client.post("/user/create-task/", data={"name": "task1",
+                                                                "creator": "1",
+                                                                "state": "2",
+                                                                "description": "description1",
+                                                                "members": "1"
+                                                                })
+        print(response)
+        self.assertEqual(Task.objects.count(), 1)
