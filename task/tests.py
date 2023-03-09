93f994c937c994529d216adabf2289c50211e102
salehshoji <mohamadsaleh.shojai@yahoo.com>
2023-03-09 12:13:55 +0330

    add create new account test

--- task/tests.py
+++ task/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from task.models import User
+
 
 # Create your tests here.
+
+class UserTest(TestCase):
+    def test_register(self):
+        # create new account
+        self.client.post("/user/signup/", data={"phone_number": "09227562938",
+                                                "username": "user1",
+                                                "firstname": "user1_firstname",
+                                                "lastname": "user1_lastname",
+                                                "password1": "a;ljf034",
+                                                "password2": "a;ljf034"
+                                                })
+        self.assertEqual(User.objects.count(), 1)
