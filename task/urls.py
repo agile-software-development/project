744142116f4c0d321203cdbd946127640df0f985
Roham Rahimi <rora1378@gmail.com>
2023-03-09 20:46:44 +0330

    add log out functionality to the site

--- task/urls.py
+++ task/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('signup/', views.register_view, name="register"),
     path('login/', views.login_view, name="login"),
+    path('logout/', views.logout_view, name="logout"),
     path('profile/', views.update_profile_view, name="profile"),
     path('create-task/', views.create_task_view, name="create-task"),
     path('tasks/', views.TaskListView.as_view(), name='list-tasks'),
