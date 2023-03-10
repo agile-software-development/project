4e93e7fca4a9cd25f2a2f32322555eeb8805daed
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2023-03-10 11:42:44 +0330

    login complete

--- task/forms.py
+++ task/forms.py
@@ -1,5 +1,8 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import *
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+
 from .models import User, Task
 
 
@@ -19,8 +22,27 @@ class UserRegisterForm(UserCreationForm):
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label='Username')
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    username = forms.CharField(label='نام کاربری')
+    password = forms.CharField(label='رمزعبور', widget=forms.PasswordInput)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Field('username', wrapper_class='mb-3', css_class='form-control', placeholder="Username"),
+            Field('password', wrapper_class='mb-3', css_class='form-control', placeholder="Username"),
+            Div(
+                Div(
+                    Submit(name='submit', value='ورود', css_class='btn-block'),
+                    css_class='grid-4'),
+                css_class='mt-4 mb-0'
+            )
+        )
+
+    class Meta:
+        labels = {
+            'username': 'نام کاربری',
+        }
 
 
 class UserProfileForm(forms.ModelForm):
