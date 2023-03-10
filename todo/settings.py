022081a05abd4729f325a351207d91707d259f45
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2023-03-10 20:55:20 +0330

    custom 404 page

--- todo/settings.py
+++ todo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-libh)roost$5g3gk9q)f(dk5pi@*k3ci=-hv43mpl$6e_*vwco'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False   # os.environ.get('IS_SANDBOX') is None
 
 ALLOWED_HOSTS = ['*']
 
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / "task/../static",
 ]
